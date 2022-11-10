4734baf688d521eed9860a56d0c44c176b87c893
lscheidl <77708814+lscheidl@users.noreply.github.com>
2022-11-09 23:41:13 -0500

    Finished studio

--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,20 @@
 // TODO: create a component that displays a single bakery item
+
+export default function BakeryItem(props) {
+    const item = props.item;
+    const updateCart = props.updateCart;
+    const cart = props.cart;
+    const handleClick = () => {
+        updateCart([...cart, item])
+    }
+    
+    return (
+        <div class="card">
+            <img src={item.image} alt="alt text"/>
+            <h3>{item.name}</h3>
+            <p>{item.description}</p>
+            <p>{item.price}</p>
+            <button onClick={handleClick} class="button">Add to Cart</button>
+        </div>
+    )
+}
